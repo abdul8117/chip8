dc2ed13b5e83d11f8b3d2d920a9f4c65295fa0eb
Abdul <abdulahad2004@yahoo.com>
2024-03-15 16:28:34 -0400

    Implement ADD instruction

--- src/chip8/Memory.java
+++ src/chip8/Memory.java
@@ -9,7 +9,7 @@ public class Memory {
 	byte[] memory = new byte[4096]; // 4 KiB of memory
 	byte[] registers = new byte[16]; // sixteen 8-bit registers
 	byte soundTimer, delayTimer; // two, special-purpose 8-bit registers
-	
+
 	short pc; // program counter
 	CallStack stack; // call stack containing up to sixteen 16-bit values
 	
@@ -18,6 +18,31 @@ public class Memory {
 	 */
 	public Memory() {
 		// ...
+	}
+
+	public decodeAndExecuteInstruction(short instruction) {
+
+		short nnn = instruction & 0xFFF;
+		byte n;
+		byte x = (instruction & 0xF00) >> 8;
+		byte y;
+		byte kk;
+
+		switch ((instruction & 0xF000) >> 12):
+			case 0x001:
+				// ...
+				break;
+			
+			// ...
+
+			case 0x007:
+
+				registers[x] += kk;
+
+				break;
+
+
+
 	}
 	
 }
