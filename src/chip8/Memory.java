f2b32d181ff0916d5434e4366d209e42f813a956
Abdul <abdulahad2004@yahoo.com>
2024-03-18 00:32:42 -0400

    Update Memory.java

--- src/chip8/Memory.java
+++ src/chip8/Memory.java
@@ -33,16 +33,81 @@ public class Memory {
 				// ...
 				break;
 			
-			// ...
+			case 0x002:
+				// Call subroutine at nnn
+				stack.push(pc);
+				pc = nnn;
+				break;
+			
+			case 0x003:
+				// Skip next instruction if Vx = kk
+				if (registers[x] == kk) 
+					pc += 2;
+				break;
+			
+			// TODO: 0x004, 0x005, 0x006
 
 			case 0x007:
-
+				// Sets Vx to Vx + kk
 				registers[x] += kk;
+				break;
+			
+			case 0x009:
+				// Skip next instruction if Vx != Vy
+				if (registers[x] != registers[y])
+					pc += 2;
+				break;
+			
+			case 0x000A:
 
+			case 0x000B:
+
+			case 0x000C:
+
+			case 0x000D:
+
+
+		
+		switch (instruction & 0xF00F):
+			case 0x8000:
+				// TODO
+				break;
+			
+			case 0x8001:
+				// TODO
 				break;
 
+			case 0x8002:
+				// TODO
+				break;
 
+			case 0x8003:
+				// TODO
+				break;
+			
+			case 0x8004:
+				// TODO
+				break;
 
-	}
+			case 0x8005:
+				// TODO
+				break;
+			
+			case 0x8006:
+				// TODO
+				break;
+
+			case 0x8007:
+				// TODO
+				break;
+
+			case 0x800E:
+				// TODO
+				break;
+		
+			}
 	
-}
+
+
+
+		}
