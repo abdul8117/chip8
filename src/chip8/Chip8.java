a38852ad152f971f5bebf16d314bdcc81adf8ff6
Abdul <abdulahad2004@yahoo.com>
2024-03-23 23:09:45 -0400

    Write Screen constructor

--- src/chip8/Chip8.java
+++ src/chip8/Chip8.java
@@ -1,10 +1,19 @@
 package chip8;
 
-public class Chip8 {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chip8 extends Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Heyo");
 		System.out.println(":3");
+
+		launch();
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		// create display, initialise keyboard and memory, load ROM (?)
+	}
 }
--- src/chip8/Screen.java
+++ src/chip8/Screen.java
@@ -1,5 +1,6 @@
 package chip8;
 
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.canvas.*;
@@ -12,27 +13,37 @@ public class Screen {
     // true = white, false = black
     private boolean[][] pixelArray = new boolean[WIDTH * SCALE][HEIGHT * SCALE];
 
+    private GraphicsContext gc;
+
     public Screen() {
-        // TODO Write constructor
+        Canvas c = new Canvas(WIDTH, HEIGHT);
+        gc = c.getGraphicsContext2D();
+        gc.fillRect(0, 0, c.getWidth(), c.getHeight()); // black screen
+        clearScreen();
+    }
 
-
-
+    /*
+     * This method simply sets every value in pixelArray to false/0/off.
+     */
+    public void clearScreen() {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                pixelArray[i][j] = false;
+            }
+        }
     }
 
     public boolean getPixel(int x, int y) {
         return pixelArray[x][y];
     }
 
+    /*
+     * This method is called when a pixel in the sprite (which is being
+     * iterated through) is 1/true/on. If the screen pixel in x and y are
+     *  also turned on, then they should be turned off, and vice versa. This
+     *  represents an XOR operation.
+     */
     public void setPixel(int x, int y) {
-        /*
-        This method is called when a pixel in the sprite (which is being
-        iterated through) is 1/true/on. If the screen pixel in x and y are
-        also turned on, then they should be turned off, and vice versa. This
-        represents an XOR operation.
-         */
-
         pixelArray[x][y] ^= true;
     }
-
-
 }
