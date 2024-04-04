7f079f4c92ad2712d6450f9df62c5ea16bd8dcfd
Abdul <abdulahad2004@yahoo.com>
2024-04-03 23:44:30 -0400

    Add StackOverflowException class

--- src/chip8/CallStack.java
+++ src/chip8/CallStack.java
@@ -1,39 +1,42 @@
 package chip8;
 
 import java.util.NoSuchElementException;
+import java.util.EmptyStackException;
+import exceptions.StackOverflowException;
 
 public class CallStack {
-	private int[] stack; // the call stack
-	private short pointer; // points to the topmost value on the stack
-	
-	/*
-	 * Constructor initialising a call stack of size 16
-	 */
-	public CallStack(int size) {
-		stack = new int[size];
-		pointer = -1; // will not point to an address if stack is empty
-	}
-	
-	public boolean isEmpty() {
-		return pointer == -1;
-	}
-	
-	public int peek() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack is empty");
-		
-		return stack[pointer];
-	}
-	
-	public void push(int address) throws Exception {
-		if (pointer >= 16) throw new Exception("Stack is full");
-		stack[++pointer] = address;
-	}
-	
-	public void pop() throws Exception {
-		if (isEmpty()) throw new Exception("Stack is already empty");
-		
-		stack[pointer] = 0;
-		pointer -= 1;
-	}
+    private int[] stack; // the call stack
+    private short pointer; // points to the topmost value on the stack
+
+    /**
+     * Constructor initialising a call stack and stack pointer.
+     */
+    public CallStack(int size) {
+        stack = new int[size];
+        pointer = -1; // will not point to an address if stack is empty
+    }
+
+    public boolean isEmpty() {
+        return pointer == -1;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack is empty");
+
+        return stack[pointer];
+    }
+
+    public void push(int address) throws StackOverflowException {
+        if (pointer >= size) throw new StackOverflowException();
+
+        stack[++pointer] = address;
+    }
+
+    public void pop() throws EmptyStackException {
+        if (isEmpty()) throw new EmptyStackException();
+
+        stack[pointer] = 0;
+        pointer -= 1;
+    }
 }
